62592ed93c08a6b9e834acb52edbe8e503ebe3a8
beznazvy777 <150190036+beznazvy777@users.noreply.github.com>
2024-06-09 23:01:08 +0300

    AnimationsObjects

--- Assets/UnitRivalManager.cs
+++ Assets/UnitRivalManager.cs
@@ -8,6 +8,9 @@ public class UnitRivalManager : MonoBehaviour
 {
     [SerializeField] private GameObject PlayerBall;
     [SerializeField] private Transform RivalIdlePoint;
+    [SerializeField] private GameObject RunAnimationFront;
+    [SerializeField] private GameObject RunAnimationBack;
+
     NavMeshAgent navMesh;
     
 
@@ -15,7 +18,7 @@ public class UnitRivalManager : MonoBehaviour
     [SerializeField] private float moveSpeed;
 
     GameManager gameManager;
-
+    float posYDestination;
     void Start()
     {
         navMesh = GetComponent<NavMeshAgent>();
@@ -34,37 +37,54 @@ public class UnitRivalManager : MonoBehaviour
 
     void Update()
     {
+        float posY = transform.position.y;
         
+
+        if (posY < posYDestination) {
+            RunAnimationFront.SetActive(true);
+            RunAnimationBack.SetActive(false);
+        }
+        if (posY >= posYDestination) {
+            RunAnimationFront.SetActive(false);
+            RunAnimationBack.SetActive(true);
+        }
+
 
         if (PlayerBall)
         {
 
             // Rotate and moving unit(rival) to target (player ball)
-            Vector3 direction = PlayerBall.transform.position - this.transform.position;
+            //Vector3 direction = PlayerBall.transform.position - this.transform.position;
             
-            float angle = Mathf.Atan2(direction.y, direction.x);
-            this.transform.rotation = Quaternion.Euler(0f,0f,angle * Mathf.Rad2Deg);
+            //float angle = Mathf.Atan2(direction.y, direction.x);
+            //this.transform.rotation = Quaternion.Euler(0f,0f,angle * Mathf.Rad2Deg);
 
 
                 
             navMesh.SetDestination(PlayerBall.transform.position);
+
+
             
-
-
         }
 
         if (!PlayerBall) {
 
 
             //Rotate and moving unit(rival) to waitPoint
-            Vector3 direction = RivalIdlePoint.position - this.transform.position;
+            //Vector3 direction = RivalIdlePoint.position - this.transform.position;
 
-            float angle = Mathf.Atan2(direction.y, direction.x);
-            this.transform.rotation = Quaternion.Euler(0f, 0f, angle * Mathf.Rad2Deg);
+            //float angle = Mathf.Atan2(direction.y, direction.x);
+            
+            //this.transform.rotation = Quaternion.Euler(0f, 0f, angle * Mathf.Rad2Deg);
             navMesh.SetDestination(RivalIdlePoint.position);
+            
         }
 
-        
+        posYDestination = transform.position.y;
+
+
+
+
     }
 
     public void OnCollisionEnter2D (Collision2D collision) {
